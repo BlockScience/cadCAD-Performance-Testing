f3dea2ea3ffc2e5f58f6ae18aa3e23a4ed2cf6bd
SeanMcOwen <seanmcowen@gmail.com>
2024-11-04 16:24:52 -0500

    Checkpoint

---
+++ src/ControlActions/Sample.py
@@ -0,0 +1,12 @@
+add_new_sample_control_action = {
+    "name": "Add New Sample Control Action",
+    "description": "Adds a new sample",
+    "constraints": [],
+    "control_action_options": [],
+    "codomain": [
+        "Sample Space",
+    ],
+    "parameters_used": ["Sample Mean", "Sample Standard Deviation"],
+}
+
+sample_control_actions = [add_new_sample_control_action]
--- src/ControlActions/__init__.py
+++ src/ControlActions/__init__.py
@@ -1,4 +1,6 @@
 from .Dummy import dummy_control_actions
+from .Sample import sample_control_actions
 
 control_actions = []
 control_actions.extend(dummy_control_actions)
+control_actions.extend(sample_control_actions)
