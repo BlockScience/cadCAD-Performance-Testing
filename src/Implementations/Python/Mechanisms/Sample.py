dca53f91ebd7db8530a97abe6edb9876b3611479
SeanMcOwen <seanmcowen@gmail.com>
2024-11-04 17:05:54 -0500

    Implement mechs

--- src/Implementations/Python/Mechanisms/Sample.py
+++ src/Implementations/Python/Mechanisms/Sample.py
@@ -1,2 +1,6 @@
-"Update Mean Mechanism",
-"Update Queue Mechanism"
+def update_mean_mechanism(state, params, spaces):
+    state["Current Mean"] = spaces[0]["mean"]
+
+
+def update_queue_mechanism(state, params, spaces):
+    state["Sample Queue"] = spaces[0]["sample_queue"]
