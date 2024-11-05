ce5da9d5fa04ff650aef9db0e3177447a7068010
SeanMcOwen <seanmcowen@gmail.com>
2024-11-04 23:35:56 -0500

    create single sim

--- simulation/postprocessing/post.py
+++ simulation/postprocessing/post.py
@@ -3,8 +3,4 @@ import pandas as pd
 
 def post_processing_function(state, params):
     df = pd.DataFrame(state["Simulation Log"])
-    df["Length (Nominal)"] = (
-        df["Length (Multiplied)"] / params["DUMMY Length Multiplier"]
-    )
-    df["D Count"] = df["Word"].apply(lambda x: x.count("D") if len(x) > 0 else None)
     return df
