5222b9bf23a2b28b4f331e71d63050a7609d5bc8
SeanMcOwen <seanmcowen@gmail.com>
2024-11-04 23:47:27 -0500

    Scaffold 2

--- model/psub.py
+++ model/psub.py
@@ -1,1 +1,15 @@
-psub_blocks = []
+def p_sample(_params, substep, state_history, state):
+    return {"Current Mean": 0, "Sample Queue": []}
+
+
+psub_blocks = [
+    {
+        "policies": {
+            "p_sample": p_sample,
+        },
+        "variables": {
+            "Current Mean": lambda a, b, c, d, e: ("Current Mean", e["Current Mean"]),
+            "Sample Queue": lambda a, b, c, d, e: ("Sample Queue", e["Sample Queue"]),
+        },
+    },
+]
